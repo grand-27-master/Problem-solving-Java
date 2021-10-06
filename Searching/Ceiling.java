45c41d443f3a82b813e01bb5ef66193e3ae50506
Vashishth gajjar <gajjarv2001@gmail.com>
2021-10-06 15:24:52 +0530

    updated

---
+++ Searching/Ceiling.java
@@ -0,0 +1,23 @@
+public class Ceiling {
+    public static void main(String[] args)
+    {
+        int[] arr={2,3,5,9,14,16,18};
+        int n=arr.length;
+        int target=15;
+        
+        int start=0,end=n-1;
+        while (start<=end) {
+            int mid=(start+end)/2;
+
+            if(arr[mid]==target)
+            System.out.print(arr[mid]);
+
+           else if(target<arr[mid])
+            end=mid-1;
+            
+            else
+            start=mid+1;
+        }
+       System.out.print(start);
+    }
+}
